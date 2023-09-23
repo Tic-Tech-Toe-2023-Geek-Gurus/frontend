f99592cb42d8e3c6dafa4e186ded4b59434089b5
natvar55 <202101402@daiict.ac.in>
2023-09-23 12:09:20 +0530

    Front-end

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,29 +1,98 @@
-import { useNavigate } from "react-router-dom";
 import "../style/Login.css";
-import { useState } from "react";
-import { record } from "vmsg";
+import { useState, useRef } from "react";
+
 export const Login = () => {
-    const navigate = useNavigate();
+
+    const [isRecording, setIsRecording] = useState(false);
+    const [audioChunks, setAudioChunks] = useState([]);
+    const [userName, setUserName] = useState(""); // State to hold the username
+    const mediaRecorder = useRef(null);
+
+    const handleStartRecording = () => {
+        if (!isRecording) {
+            setAudioChunks([]);
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then((stream) => {
+                    mediaRecorder.current = new MediaRecorder(stream);
+                    mediaRecorder.current.ondataavailable = (e) => {
+                        if (e.data.size > 0) {
+                            setAudioChunks((chunks) => [...chunks, e.data]);
+                        }
+                    };
+                    mediaRecorder.current.start();
+                    setIsRecording(true);
+                })
+                .catch((error) => {
+                    console.error('Error accessing microphone:', error);
+                });
+        } else {
+            mediaRecorder.current.stop();
+            setIsRecording(false);
+        }
+    };
+
+    const handleDownload = () => {
+        const blob = new Blob(audioChunks, { type: 'audio/wav' });
+
+        // Create a FormData object to send the audio file and username
+        const formData = new FormData();
+        formData.append('audio', blob);
+        formData.append('userName', userName);
+
+        // Replace 'your-api-endpoint' with the actual URL of your API endpoint
+        fetch('your-api-endpoint', {
+            method: 'POST',
+            body: formData
+        }).then((response) => {
+                if (response.ok) {
+                    console.log('Audio and username sent successfully.');
+                    // Handle success as needed
+                } else {
+                    console.error('Failed to send audio and username to the API.');
+                    // Handle error as needed
+                }
+            })
+            .catch((error) => {
+                console.error('Error sending audio and username:', error);
+                // Handle error as needed
+            });
+    };
+
     return (
         <div class="wrapper">
             <div class="logo">
-                <img src="https://cdn2.vectorstock.com/i/1000x1000/40/56/speech-recognition-glyph-icon-vector-28874056.jpg" alt=""/>
+                <img src="https://cdn2.vectorstock.com/i/1000x1000/40/56/speech-recognition-glyph-icon-vector-28874056.jpg" alt="" />
             </div>
             <div class="text-center mt-3 name">
                 Verify your Voice
             </div>
-            <form class="p-3 mt-3">
+            <div class="p-3 mt-3">
                 <div class="form-field d-flex align-items-center">
                     <span class="far fa-user"></span>
-                    <input type="text" name="userName" id="userName" placeholder="Username"/>
+                    <input type="text" name="userName" id="userName" placeholder="Username" value={userName} onChange={(e) => setUserName(e.target.value)} />
                 </div>
-                <div class="record d-flex align-items-center">
-                    <button class="btn mt-3">Start Recording</button>
-                </div>
-                <button class="btn mt-3">Verify</button>
-            </form>
+                <div className="record d-flex align-items-center">
+                {audioChunks.length === 0 ? (
+                    <button onClick={handleStartRecording} className="btn mt-3">
+                        {isRecording ? 'Stop Recording' : 'Start Recording'}
+                    </button>
+                ) : (
+                    <div>
+                        <audio controls>
+                            <source src={URL.createObjectURL(new Blob(audioChunks, { type: 'audio/wav' }))} type="audio/wav" />
+                        </audio>
+                        <button onClick={handleStartRecording} className="record btn mt-3">
+                            Re-record
+                        </button>
+                        <button onClick={handleDownload} className="btn mt-3">
+                            Verify
+                        </button>
+                    </div>
+                )}
+            </div>
+            </div>
             <div class="text-center fs-6">
-            Register new account? <a href="#">Sign up</a>
+                Register new account? <a href="/signup">Sign up</a>
             </div>
         </div>
     )
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,26 +1,133 @@
 import "../style/Signup.css";
+import { useState, useRef } from "react";
+const path = require('path');
 export const Signup = () => {
+    const [isRecording1, setIsRecording1] = useState(false);
+    const [audioChunks1, setAudioChunks1] = useState([]);
+    const [isRecording2, setIsRecording2] = useState(false);
+    const [audioChunks2, setAudioChunks2] = useState([]);
+    const [userName, setUserName] = useState("");
+    const mediaRecorder = useRef(null);
+
+    const handleStartRecording1 = () => {
+        if (!isRecording1) {
+            setAudioChunks1([]);
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then((stream) => {
+                    mediaRecorder.current = new MediaRecorder(stream);
+                    mediaRecorder.current.ondataavailable = (e) => {
+                        if (e.data.size > 0) {
+                            setAudioChunks1((chunks) => [...chunks, e.data]);
+                        }
+                    };
+                    mediaRecorder.current.start();
+                    setIsRecording1(true);
+                })
+                .catch((error) => {
+                    console.error('Error accessing microphone:', error);
+                });
+        } else {
+            mediaRecorder.current.stop();
+            setIsRecording1(false);
+        }
+    };
+
+    const handleStartRecording2 = () => {
+        if (!isRecording2) {
+            setAudioChunks2([]);
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then((stream) => {
+                    mediaRecorder.current = new MediaRecorder(stream);
+                    mediaRecorder.current.ondataavailable = (e) => {
+                        if (e.data.size > 0) {
+                            setAudioChunks2((chunks) => [...chunks, e.data]);
+                        }
+                    };
+                    mediaRecorder.current.start();
+                    setIsRecording2(true);
+                })
+                .catch((error) => {
+                    console.error('Error accessing microphone:', error);
+                });
+        } else {
+            mediaRecorder.current.stop();
+            setIsRecording2(false);
+        }
+    };
+    const handleDownload = () => {
+        const blob = new Blob(audioChunks1, { type: 'audio/wav' });
+
+        // Create a FormData object to send the audio file and username
+        const formData = new FormData();
+        formData.append('audio', blob);
+        formData.append('userName', userName);
+
+        // Replace 'your-api-endpoint' with the actual URL of your API endpoint
+        fetch('your-api-endpoint', {
+            method: 'POST',
+            body: formData
+        }).then((response) => {
+            if (response.ok) {
+                console.log('Audio and username sent successfully.');
+                // Handle success as needed
+            } else {
+                console.error('Failed to send audio and username to the API.');
+                // Handle error as needed
+            }
+        })
+            .catch((error) => {
+                console.error('Error sending audio and username:', error);
+                // Handle error as needed
+            });
+    };
+
     return (
         <div class="wrapper">
             <div class="logo">
-                <img src="https://cdn2.vectorstock.com/i/1000x1000/40/56/speech-recognition-glyph-icon-vector-28874056.jpg" alt=""/>
+                <img src={path.resolve(__dirname,"../../public/speech-recognition-icon.jpg")}  />
             </div>
             <div class="text-center mt-3 name">
                 Signup
             </div>
-            <form class="p-3 mt-3">
+            <div class="p-3 mt-3">
                 <div class="form-field d-flex align-items-center">
                     <span class="far fa-user"></span>
-                    <input type="text" name="userName" id="userName" placeholder="Username"/>
+                    <input type="text" name="userName" id="userName" placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
                 </div>
                 <div class="record d-flex align-items-center">
-                    <button class="btn mt-3">Start_Recording For data1</button>
+                    {audioChunks1.length === 0 ? (
+                        <button onClick={handleStartRecording1} className="btn mt-3">
+                            {isRecording1 ? 'Stop Recording For data1' : 'Start Recording For data1'}
+                        </button>
+                    ) : (
+                        <div>
+                            <audio controls>
+                                <source src={URL.createObjectURL(new Blob(audioChunks1, { type: 'audio/wav' }))} type="audio/wav" />
+                            </audio>
+                            <button onClick={handleStartRecording1} className="rerecord btn mt-3">
+                                Re-record data1
+                            </button>
+                        </div>
+                    )}
                 </div>
                 <div class="record d-flex align-items-center">
-                    <button class="btn mt-3">Start_Recording For data2</button>
+                    {audioChunks2.length === 0 ? (
+                        <button onClick={handleStartRecording2} className="btn mt-3">
+                            {isRecording2 ? 'Stop Recording For data2' : 'Start Recording For data2'}
+                        </button>
+                    ) : (
+                        <div>
+                            <audio controls>
+                                <source src={URL.createObjectURL(new Blob(audioChunks2, { type: 'audio/wav' }))} type="audio/wav" />
+                            </audio>
+                            <button onClick={handleStartRecording2} className="rerecord btn mt-3">
+                                Re-record data2
+                            </button>
+                        </div>
+                    )}
                 </div>
-                <button class="btn mt-3">Register</button>
-            </form>
+                <button class="btn mt-3" onClick={handleDownload}>Register</button>
+            </div>
         </div>
     );
 };
