39be3894a2e842832d092ea277403d8008ac3c28
natvar55 <202101402@daiict.ac.in>
2023-09-23 15:26:08 +0530

    Nav updated

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,11 +1,17 @@
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import "../style/Nav.css";
 export const Nav = () => {
+    const navigate=useNavigate();
+    const logout=()=>
+    {
+        localStorage.clear();
+        navigate('/');
+    }
+    const auth=localStorage.getItem('user');
     return (
         <div className="navbar">
-        <Link to="/Home"> Home </Link>
-        <Link to="/signup"> Signup </Link>
-        <Link to="/Login"> Login </Link>
+            {auth?<><Link to="/Home"> Home </Link><Link onClick={logout}> Logout </Link></>:<><Link to="/"> Home </Link><Link to="/signup"> Signup </Link><Link to="/Login"> Login </Link></>}
+        
         </div>    
     )
 };
