3ef648719481226b57fe12138b36eb3652643383
natvar55 <202101402@daiict.ac.in>
2023-09-23 22:15:28 +0530

    Profile Page added

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,45 +1,102 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import "../style/Profile.css";
+import { useNavigate } from "react-router-dom";
 export const Profile = () => {
+    const navigate = useNavigate();
+
+    const handleCancel = () => {
+        navigate('/');
+    }
+
+    const schema = yup.object().shape({
+        name: yup.string().required("Your First Name is Required!"),
+        age: yup.number().positive().integer().min(18).required(),
+        email: yup.string().email().required(),
+        mobileno: yup.string().min(10, "Mobile No must be at least 10 digits")
+            .max(10, "Mobile No cannot be more than 10 digits")
+            .required("Mobile No is Required"),
+        country: yup.string().required(),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const onsubmit = async (data) => {
+        const result = await fetch('your-api-endpoint', {
+            method: 'POST',
+            body: JSON.parse(JSON.stringify(formData))
+        }).then((response) => {
+            if (response.ok) {
+                alert('Data updated successfully.');
+                // Handle success as needed
+            } else {
+                console.error('Failed to send data to the API.');
+            }
+        }).catch((error) => {
+                console.error('Error sending data:', error);
+                // Handle error as needed
+            });
+        localStorage.setItem("userdata",data);
+        navigate('/');
+    }
+
     return (
-        <div class="container rounded bg-white mt-5 mb-5">
-            <div class="row">
-                <div class="col-md-3 border-right">
-                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" alt=" " /><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
-                </div>
-                <div class="col-md-5 border-right">
-                    <div class="p-3 py-5">
-                        <div class="d-flex justify-content-between align-items-center mb-3">
-                            <h4 class="text-right">Profile Settings</h4>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value="" /></div>
-                            <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname" /></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value="" /></div>
-                            <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value="" /></div>
-                            <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                            <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                            <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                            <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                            <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value="" /></div>
-                            <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value="" /></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value="" /></div>
-                            <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state" /></div>
-                        </div>
-                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="p-3 py-5">
-                        <div class="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Experience</span></div>
-                        <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value="" /></div>
-                            <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value="" /></div>
-                    </div>
-                </div>
+        <div>
+            <div class="user">
+                <img className="avatar" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin" />
             </div>
-        </div>
+            <h5 class="user-name">Yuki Hayashi</h5>
+            <h2>Personal info</h2>
+            <form onSubmit={handleSubmit(onsubmit)} class="form-horizontal" role="form">
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Name:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" defaultValue={"User"} {...register("name")} />
+                    </div>
+                    <p>{errors.first_name?.message}</p>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Age:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" defaultValue={18} {...register("age")} />
+                    </div>
+                    <p>{errors.last_name?.message}</p>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Mobile No:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" defaultValue={"4518484841"} {...register("mobileno")} />
+                    </div>
+                    <p>{errors.mobileno?.message}</p>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Email:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" defaultValue={"India@gmail.com"} {...register("email")} />
+                    </div>
+                    <p>{errors.email?.message}</p>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-3 control-label">Country:</label>
+                    <div class="col-lg-8">
+                        <input class="form-control" type="text" defaultValue={"India"} {...register("country")} />
+                    </div>
+                    <p>{errors.Country?.message}</p>
+                </div>
+                <div class="form-group">
+                    <button onClick={handleCancel} className="btn">
+                        Cancel
+                    </button>
+                    <input type="submit" className="btn" value="Update" />
+                </div>
+            </form>
+        </div >
     );
 };
